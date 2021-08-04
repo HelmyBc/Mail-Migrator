6d233b657a16400a0278c9e9b5a894d54642f5be
HelmyBc <helmibouchiha@gmail.com>
2021-08-04 18:28:42 +0100

    All data in 1 JSON File

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ $.getJSON('data.JSON',function(destination_server_type){
         $select.append('<option id="'+"i"+'">'+destination_server_type['destination'][i]['name']+'</option>')
    }
 });
+
+var $selectIcon=$('#social__icons');
+
+$.getJSON('data.JSON',function(social__icons){
+    $selectIcon.html('');
+    for ( var i = 0 ; i < social__icons['iconLinks'].length ; i++){
+        $selectIcon.append('<a  class="social__icon--link" target="_blank" arial-label=" '+social__icons['iconLinks'][i]['aria-label'] +'" href= "'+social__icons['iconLinks'][i]['href']+'">'+'<i class= "'+social__icons['iconLinks'][i]['class']+'"></i></a>')
+   }
+});
