46d7811d7cb8f10990943dd03b9626d5d817672a
HelmyBc <helmibouchiha@gmail.com>
2021-08-06 12:51:43 +0100

    ready to link with backend

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
 var $selectsr=$('#source_server_type');
-
+var $selectsri=$('#autofill1');
 $.getJSON('data.JSON',function(source_server_type){
-    $selectsr.html('');
-    for ( var i = 0 ; i < source_server_type['source'].length ; i++){
-        $selectsr.append('<option id="'+i+'">'+source_server_type['source'][i]['name']+'</option>')
-    }
+$selectsr.html('');
+for ( var i = 0 ; i < source_server_type['source'].length ; i++){
+    $selectsr.append('<option id="'+i+'" value="'+i+'">'+source_server_type['source'][i]['name']+'</option>');}
+
+          $("#source_server_type").change(function(){
+            var x=this.options[this.selectedIndex].id;
+            document.getElementById('autofill1').value=source_server_type['source'][x]['url'];
+           
+          })
 });
+
+
+
+
+//document.getElementById('source_server_type').value
+
+
+
 
 var $select=$('#destination_server_type');
-
+var $selecti=$('#autofill2');
 $.getJSON('data.JSON',function(destination_server_type){
-    $select.html('');
+$select.html('');
     for ( var i = 0 ; i < destination_server_type['destination'].length ; i++){
-        $select.append('<option id="'+"i"+'">'+destination_server_type['destination'][i]['name']+'</option>')
-   }
+        $select.append('<option id="'+ i*10 +'" value="'+ i + '">'+destination_server_type['destination'][i]['name']+'</option>');}
+   
+        $("#destination_server_type").change(function(){
+        var y=this.options[this.selectedIndex].id;
+        document.getElementById('autofill2').value=destination_server_type['destination'][y/10]['url'];
+   
+  })
+
 });
 
+
+/*var $selectsri=$('#autofill1');
+$.getJSON('data.JSON',function(source_server_type){
+    switch(document.getElementById('source_server_type').value) {
+        case "0":
+            document.getElementById('autofill1').value="aaaa";
+          break;*/
 var $selectIcon=$('#social__icons');
 
+      
 $.getJSON('data.JSON',function(social__icons){
     $selectIcon.html('');
     for ( var i = 0 ; i < social__icons['iconLinks'].length ; i++){
-        $selectIcon.append('<a  class="social__icon--link" target="_blank" arial-label=" '+social__icons['iconLinks'][i]['aria-label'] +'" href= "'+social__icons['iconLinks'][i]['href']+'">'+'<i class= "'+social__icons['iconLinks'][i]['class']+'"></i></a>')
+        $selectIcon.append('<a  class="social__icon--link" target="_blank" arial-label=" '+social__icons['iconLinks'][i]['aria-label'] +'" href= "'+social__icons['iconLinks'][i]['href']+'">'+'<i class= "'+social__icons['iconLinks'][i]['class']+'"></i></a>');
    }
 });
